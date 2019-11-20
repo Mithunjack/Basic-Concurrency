147ee95bee5259fbfc493d10a2c8e04dd562a6ba
Mithun Das <mithund654@gmail.com>
2019-11-20 17:24:47 +0100

    using semaphore

--- src/com/company/Professor.java
+++ src/com/company/Professor.java
@@ -1,35 +1,33 @@
 package com.company;
 
-public class Professor extends Thread{
-    Stick left,right;
+public class Professor extends Thread {
+    Stick left, right;
     int id;
-    Professor(int id,Stick left, Stick right){
+
+    Professor(int id, Stick left, Stick right) {
         this.left = left;
         this.right = right;
         this.id = id;
     }
-    public void run(){
+
+    public void run() {
         eat();
     }
+
     public void eat() {
         while (true) {
-            synchronized (this.left) {
-                System.out.println(id + " left one picked");
-                this.left.pickStick();
-                if (this.right.isAvailable()){
-                    synchronized (this.right) {
-                        System.out.println(id + " right one picked");
-                        this.right.pickStick();
-                        System.out.println(id + " Left one dropped");
-                        this.left.dropStick();
-                        System.out.println(id + " right one dropped");
-                        this.right.dropStick();
-                    }
-                }
-                else {
-                    System.out.println(id + " Left one dropped");
-                    this.left.dropStick();
-                }
+            System.out.println(id + " left one picked");
+            this.left.pickStick();
+            if (this.right.isAvailable()) {
+                System.out.println(id + " right one picked");
+                this.right.pickStick();
+                System.out.println(id + " Left one dropped");
+                this.left.dropStick();
+                System.out.println(id + " right one dropped");
+                this.right.dropStick();
+            } else {
+                System.out.println(id + " Left one dropped");
+                this.left.dropStick();
             }
         }
     }
--- src/com/company/Stick.java
+++ src/com/company/Stick.java
@@ -2,14 +2,24 @@ package com.company;
 import java.util.concurrent.Semaphore;
 
 public class Stick{
-    boolean available=true;
+    Semaphore sem;
+    boolean available;
+    Stick(){
+        this.sem = new Semaphore(1);
+    }
     public synchronized void pickStick(){
     //    System.out.println("Status:pick");
+        try {
+            sem.acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         available = false;
     }
 
     public synchronized void dropStick(){
   //      System.out.println("Status: drop");
+        sem.release();
         available = true;
     }
 
